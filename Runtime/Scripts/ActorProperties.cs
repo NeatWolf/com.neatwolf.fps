cb781872dcdbc127850a0bde086b9aea43cdf70d
NeatWolf <alessandro.salvati.github@gmail.com>
2023-08-02 23:45:11 +0100

    Refactored actor properties and player look scripts for better organization.

--- Runtime/Scripts/ActorProperties.cs
+++ Runtime/Scripts/ActorProperties.cs
@@ -5,9 +5,16 @@ namespace NeatWolf.FPS
     [CreateAssetMenu(fileName = "ActorProperties", menuName = "Actor/Properties", order = 0)]
     public class ActorProperties : ScriptableObject
     {
+        [Tooltip("The base movement speed of the actor.")]
         public float baseMovementSpeed = 2f;
+
+        [Tooltip("The influence of gravity on the actor.")]
         public float gravityInfluence = 1f;
+
+        [Tooltip("The acceleration curve for the actor's movement.")]
         public AnimationCurve movementAccelerationCurve = AnimationCurve.EaseInOut(0, 0, 1, 1);
+
+        [Tooltip("The time it takes for the actor to reach maximum acceleration.")]
         public float movementAccelerationTime = 0.1f;
     }
 }
--- Runtime/Scripts/PlayerLook.cs
+++ Runtime/Scripts/PlayerLook.cs
@@ -2,46 +2,61 @@ using UnityEngine;
 
 namespace NeatWolf.FPS
 {
+    /// <summary>
+    /// Controls the player's camera look movement.
+    /// </summary>
     public class PlayerLook : MonoBehaviour
     {
         [SerializeField]
+        [Tooltip("The input handler for player look input. Handles mouse or joystick input.")]
         private PlayerInputHandler inputHandler;
 
         [SerializeField]
+        [Tooltip("The sensitivity of the player's look movement. Higher values result in faster camera movement. Valid range: 0.1 to 10.")]
+        [Range(0.1f, 10f)]
         private float sensitivity = 2f;
 
         [SerializeField]
+        [Tooltip("The lower angle limit for vertical look rotation. Specifies the lowest angle the camera can look downwards. Valid range: -90 to 0.")]
+        [Range(-90f, 0f)]
         private float lowerAngleLimit = -60f;
 
         [SerializeField]
+        [Tooltip("The upper angle limit for vertical look rotation. Specifies the highest angle the camera can look upwards. Valid range: 0 to 90.")]
+        [Range(0f, 90f)]
         private float upperAngleLimit = 60f;
 
         [SerializeField]
+        [Tooltip("The animation curve for horizontal look speed. Allows customizing the speed curve for horizontal camera movement.")]
         private AnimationCurve horizontalSpeedCurve = AnimationCurve.EaseInOut(0f, 0f, 1f, 1f);
 
         [SerializeField]
+        [Tooltip("The animation curve for vertical look speed. Allows customizing the speed curve for vertical camera movement.")]
         private AnimationCurve verticalSpeedCurve = AnimationCurve.EaseInOut(0f, 0f, 1f, 1f);
 
         private float verticalLookRotation;
 
-        [SerializeField] 
+        [SerializeField]
+        [Tooltip("The root transform for horizontal rotation. Specifies the transform that rotates horizontally along with the player.")]
         private Transform HorizontalRotationRoot;
 
-        // Smoothing time for camera movements.
-        // This value is a rough estimate of the time the smoothing takes to occur (in seconds).
-        // It's calculated based on the frame rate and the desired number of frames over which to smooth.
-        // For example, if targeting 60 FPS (where each frame is ~0.01667 seconds) and we want smoothing
-        // to occur over approximately 2 frames, smoothTime could be set to 0.01667 * 2 = 0.03334 seconds.
-        // The actual feel and results may vary depending on the game's logic and performance,
-        // so testing and adjusting is necessary.
-        private float smoothTime = 0.03334f; 
+        [SerializeField]
+        [Tooltip("The smoothing time for camera movements. Determines how quickly the camera movement is smoothed. Valid range: 0.01 to 1.")]
+        [Range(0.01f, 1f)]
+        private float smoothTime = 0.03334f;
 
+        /// <summary>
+        /// Updates the camera look movement.
+        /// </summary>
         void Update()
         {
             Look();
             LockCursor();
         }
 
+        /// <summary>
+        /// Performs the camera look movement based on input.
+        /// </summary>
         void Look()
         {
             Vector2 lookInput = inputHandler.GetLook();
@@ -60,6 +75,9 @@ namespace NeatWolf.FPS
             transform.localEulerAngles = Vector3.left * verticalLookRotation;
         }
 
+        /// <summary>
+        /// Locks or unlocks the cursor based on application focus.
+        /// </summary>
         void LockCursor()
         {
             if (Application.isFocused)
